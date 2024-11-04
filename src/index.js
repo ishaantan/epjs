6da32e01c656d741b465e8162c8dcef35bb614f8
Thomas Hoang <thomas.h@webprovise.com>
2024-11-04 22:20:09 +0700

    check

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const route = require("./routes");
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+            app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("combined"));
